7eada37538c15157bc6d4abda0da1a01e2e8363c
Prince Yadav <prinsyadac@gmail.com>
2025-10-06 22:27:54 +0530

    feat: add routes for task management including getTasks and taskToggleComplete

--- backend/src/routes/task.route.js
+++ backend/src/routes/task.route.js
@@ -1,8 +1,29 @@
 import { Router } from "express";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
+import { validate } from "../middlewares/validator.middleware.js";
+import {
+     createTask,
+    updateTask,
+    deleteTask,
+    getTeskById,
+    getTasks,
+    taskToggleComplete
+} from "../controllers/task.controller.js"
+import {taskCreateValidator} from "../validators/index.js"
+
 
 
 const router = Router()
+
+router.route('/gp')
+    .get(verifyJWT,getTasks)
+    .post(taskCreateValidator(),validate, verifyJWT,createTask)
+
+router.route('/:id/gp')
+    .get(verifyJWT,getTeskById)
+    .put(verifyJWT,updateTask)
+    .delete(verifyJWT,deleteTask)
+    .post(verifyJWT,taskToggleComplete)
 
 
 export default router;
