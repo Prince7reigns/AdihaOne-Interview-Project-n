81d87274d6fa47d7171a852061b9700dce90484b
Prince Yadav <prinsyadac@gmail.com>
2025-10-07 13:06:39 +0530

    bug

--- backend/src/app.js
+++ backend/src/app.js
@@ -11,7 +11,7 @@ app.use(cookieparser())
 
 // cors config
 app.use(cors({
-    origin:[process.env.CORS_ORIGIN,"https://task-manager-fbg3.onrender.com"],
+    origin:["http://localhost:5173","https://task-manager-fbg3.onrender.com"],
     credentials: true,
     methods:["GET","POST","PUT","PATCH","DELETE","OPTIONS"],
     allowedHeaders:["Content-Type","Authorization"]
