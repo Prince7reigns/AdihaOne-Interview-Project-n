dbb4a8b2143ec32f19eafaa73d6799d35c8ada75
Prince Yadav <prinsyadac@gmail.com>
2025-10-07 12:11:27 +0530

    cors change

--- backend/src/app.js
+++ backend/src/app.js
@@ -11,7 +11,7 @@ app.use(cookieparser())
 
 // cors config
 app.use(cors({
-    origin:process.env.CORS_ORIGIN?.split(",") || "http://localhost:5173",
+    origin:process.env.CORS_ORIGIN,
     Credential:true,
     methods:["GET","POST","PUT","PATCH","DELETE","OPTIONS"],
     allowedHeaders:["Content-Type","Authorization"]
