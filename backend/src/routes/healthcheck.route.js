4fad9015a663d8e851c5f165819c252ff195863c
Prince Yadav <prinsyadac@gmail.com>
2025-10-08 12:53:58 +0530

    fix: add JWT verification to healthcheck route

--- backend/src/routes/healthcheck.route.js
+++ backend/src/routes/healthcheck.route.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { healthcheck } from "../controllers/healthcheck.controller.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 
 const router = Router()
 
-router.route("/").get(healthcheck)
+router.route("/").get(verifyJWT,healthcheck)
 
 export default router
